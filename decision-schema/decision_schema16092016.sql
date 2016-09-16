1b7e8919a6762b9d91d5a2ebbb15d1161b138841
leitediogo <leitediogo@gmail.com>
2016-09-16 17:20:18 +0100

    mysql schema added

---
+++ decision-schema/decision_schema16092016.sql
@@ -0,0 +1,32 @@
+drop table if exists choice;
+drop table if exists criteria;
+drop table if exists decision;
+
+CREATE TABLE decision (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+description VARCHAR(200) NOT NULL,
+status VARCHAR(30),
+creation_date TIMESTAMP,
+last_update_date TIMESTAMP
+);
+
+CREATE TABLE choice (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+decision_id INT(6),
+name VARCHAR(30) NOT NULL,
+description VARCHAR(200) NOT NULL,
+status VARCHAR(30),
+creation_date TIMESTAMP,
+last_update_date TIMESTAMP
+);
+
+CREATE TABLE criteria (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+decision_id INT(6),
+name VARCHAR(30) NOT NULL,
+description VARCHAR(200) NOT NULL,
+status VARCHAR(30),
+creation_date TIMESTAMP,
+last_update_date TIMESTAMP
+);
